b1a8022695cac4a19f3f584a8f7b72972a14348a
James Arney <wjarney@gmail.com>
2020-02-28 01:52:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,11 @@
-# Define display_board that accepts a board and prints
-display_board(board)
-board = [" "," "," "," "," "," "," "," "," "]
+def display_board(board)
   puts " #{board(0)} | #{board(1)} | #{board(2)} "
   puts "-----------"
   puts " #{board(3)} | #{board(4)} | #{board(5)} "
   puts "-----------"
   puts " #{board(6)} | #{board(7)} | #{board(8)} "
+
+end
+
+# display_board(board)
+# board = [" "," "," "," "," "," "," "," "," "]
