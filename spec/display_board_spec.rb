f405c63fb03442f596faedd5fea9484936ad7ff3
James Arney <wjarney@gmail.com>
2020-02-28 02:09:29 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -21,7 +21,7 @@ describe "#display_board in 'lib/display_board.rb" do
 
       output = capture_puts{ display_board(board) }
       rows = output.split("\n")
-
+      binding.pry
       expect(rows[0]).to eq("   |   |   ")
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq("   | X |   ")
