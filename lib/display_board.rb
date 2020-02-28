166125c66a7f0497be842986e54ba42f7ca62bec
James Arney <wjarney@gmail.com>
2020-02-28 02:14:51 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,3 +9,13 @@ end
 
 # display_board(board)
 # def board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+  puts ``` #{board(0)} | #{board(1)} | #{board(2)} 
+  -----------
+  #{board(3)} | #{board(4)} | #{board(5)}
+  -----------
+  #{board(6)} | #{board(7)} | #{board(8)}
+  ```
+
+end
