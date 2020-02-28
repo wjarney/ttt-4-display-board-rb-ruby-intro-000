e8c69ad074e339cfd94aab82028166a067228289
James Arney <wjarney@gmail.com>
2020-02-28 02:17:22 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
-def display_board(board)
-  puts " #{board(0)} | #{board(1)} | #{board(2)} "
+def display_board(board_1)
+  puts " #{board_1(0)} | #{board_1(1)} | #{board_1(2)} "
   puts "-----------"
-  puts " #{board(3)} | #{board(4)} | #{board(5)} "
+  puts " #{board_1(3)} | #{board_1(4)} | #{board_1(5)} "
   puts "-----------"
-  puts " #{board(6)} | #{board(7)} | #{board(8)} "
+  puts " #{board_1(6)} | #{board_1(7)} | #{board_1(8)} "
 
 end
 
